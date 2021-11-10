b8b5e1b0525e7801bb2fd3b338acb30d708fc7b3
CoderKeung <coderkeung@gmail.com>
2021-11-10 22:01:16 +0800

    Create My Theme First Demo

--- static/js/script.js
+++ static/js/script.js
@@ -11,23 +11,78 @@ function smoothScrollTop(){
         }
     });
 }
-/*
 
-var isOn = false;
+let tabsNav = document.getElementById("tabsNav")
 
-function switchFont (){
-    let sunIcon = document.getElementById('sunIcon')
-    let switchOn = document.getElementById('switchFont')
-    if (switchOn.className === "switchOn") {
-        switchOn.innerHTML = feather.icons['moon'].toSvg();
-        switchOn.className = "switchOff";
-        document.documentElement.setAttribute('data-theme', ['dark', 'light'][+isOn]);
-        isOn = !isOn;
-    } else {
-        switchOn.innerHTML = feather.icons['sun'].toSvg();
-        switchOn.className = "switchOn";
-        document.documentElement.setAttribute('data-theme', ['dark', 'light'][+isOn]);
-        isOn = !isOn;
+let recentArticleButton = document.getElementById("recentArticle-button")
+let tagsButton = document.getElementById("tags-button")
+let allArticleButton = document.getElementById("allArticle-button")
+
+recentArticleButton.addEventListener("click",function (){
+    if (this.className === "tab")
+        this.className += " tabClick"
+    clearTabClick(getClearClass(this.id))
+    document.getElementById(getTabClick()).style.display = "block";
+    hideClass(getHideClass(getTabClick()));
+})
+
+tagsButton.addEventListener("click",function (){
+    if (this.className === "tab")
+        this.className += " tabClick"
+    clearTabClick(getClearClass(this.id))
+    document.getElementById(getTabClick()).style.display = "block";
+    hideClass(getHideClass(getTabClick()));
+})
+
+allArticleButton.addEventListener("click",function (){
+    if (this.className === "tab")
+        this.className += " tabClick"
+    clearTabClick(getClearClass(this.id))
+    document.getElementById(getTabClick()).style.display = "block";
+    hideClass(getHideClass(getTabClick()));
+})
+
+
+function getClearClass(idName) {
+    switch (idName) {
+        case "recentArticle-button":
+            return ["tags-button","allArticle-button"];
+        case "tags-button":
+            return ["recentArticle-button","allArticle-button"];
+        case "allArticle-button":
+            return ["recentArticle-button","tags-button"];
     }
 }
-*/
+function  clearTabClick(i) {
+    for (const iKey in i) {
+        document.getElementById(i[iKey]).className = "tab"
+    }
+}
+
+function getTabClick(){
+    let tabClick = document.getElementsByClassName("tabClick")
+    switch (tabClick[0].id) {
+        case "recentArticle-button":
+            return "recentArticle";
+        case "tags-button":
+            return "tags";
+        case "allArticle-button":
+            return "allArticle";
+    }
+}
+function getHideClass(idName) {
+    switch (idName) {
+        case "recentArticle":
+            return ["tags","allArticle"];
+        case "tags":
+            return ["recentArticle","allArticle"];
+        case "allArticle":
+            return ["recentArticle","tags"];
+    }
+}
+
+function hideClass(i) {
+    for (const iKey in i) {
+        document.getElementById(i[iKey]).style.display = "none"
+    }
+}
